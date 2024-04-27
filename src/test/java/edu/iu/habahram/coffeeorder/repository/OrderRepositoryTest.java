c2d336aaeda5ba282262a34d29d293154ddbe75d
StratiKool <skoulour@iu.edu>
2024-04-27 00:12:25 -0400

    HW 9 PostgreSQL Commit

--- src/test/java/edu/iu/habahram/coffeeorder/repository/OrderRepositoryTest.java
+++ src/test/java/edu/iu/habahram/coffeeorder/repository/OrderRepositoryTest.java
@@ -10,5 +10,12 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderRepositoryTest {
+    private static final String DATABASE_NAME = "db.txt";
 
+    @Test
+    void add1() throws Exception {
+        int one = 1;
+        int two = 2;
+        assertNotEquals(one, two);
+    }
 }
